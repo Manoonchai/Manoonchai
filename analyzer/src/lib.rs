2dac24419e2a9aef966655dd14f2c046de80d971
Manassarn Manoonchai <manassarn@gmail.com>
2020-10-25 00:15:59 +0700

    feat: output result sorted by frequency

--- analyzer/src/lib.rs
+++ analyzer/src/lib.rs
@@ -8,7 +8,7 @@ pub fn run() -> bool {
     true
 }
 
-fn analyze_string(str: &str) -> HashMap<String, u32> {
+pub fn analyze_string(str: &str) -> HashMap<String, u32> {
     let mut output: HashMap<String, u32> = HashMap::new();
 
     for ch in str.chars() {
@@ -17,6 +17,14 @@ fn analyze_string(str: &str) -> HashMap<String, u32> {
     }
 
     output
+}
+
+pub fn analyze_string_sorted(str: &str) -> Vec<(String, u32)> {
+    let result: HashMap<String, u32> = analyze_string(&str);
+    let mut result_vec: Vec<(String, u32)> = result.into_iter().collect();
+    result_vec.sort_by(|a, b| b.1.cmp(&a.1));
+
+    result_vec
 }
 
 #[cfg(test)]
@@ -42,12 +50,22 @@ mod tests {
     }
 
     #[test]
-        fn test_analyze_string_thai() {
+    fn test_analyze_string_thai() {
         let output = analyze_string("สวัสดี");
         assert_eq!(output["ส"], 2);
         assert_eq!(output["ว"], 1);
         assert_eq!(output["ั"], 1);
         assert_eq!(output["ด"], 1);
         assert_eq!(output["ี"], 1);
+    }
+
+    #[test]
+    fn test_analyze_string_sorted() {
+        let output = analyze_string_sorted("544aa22a2211111");
+        assert_eq!(output[0], ("1".to_string(), 5));
+        assert_eq!(output[1], ("2".to_string(), 4));
+        assert_eq!(output[2], ("a".to_string(), 3));
+        assert_eq!(output[3], ("4".to_string(), 2));
+        assert_eq!(output[4], ("5".to_string(), 1));
     }
 }
--- analyzer/src/main.rs
+++ analyzer/src/main.rs
@@ -1,8 +1,17 @@
 mod lib;
 
+use std::fs;
+use std::path::Path;
 use lib::*;
 
 fn main() {
     println!("Hello, world!");
-    run();
+
+    let filename = Path::new("data/thai1k.txt");
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    let result = analyze_string_sorted(contents.as_str());
+
+    println!("{:?}", result);
 }
