a32ed525f62e9af3449707fd4eb8fe4d3b7d1c8b
Manassarn Manoonchai <manassarn@gmail.com>
2020-11-02 23:06:39 +0700

    feat: process triads & add Wongnai, Wisesight corpus

--- analyzer/src/lib.rs
+++ analyzer/src/lib.rs
@@ -26,12 +26,27 @@ pub fn analyze_string_sorted(str: &str) -> Vec<(String, u32)> {
     result_vec
 }
 
+#[allow(dead_code)]
 fn analyze_string_triads(str: &str) -> HashMap<String, u32> {
     let mut output: HashMap<String, u32> = HashMap::new();
 
-    let strArray = str.chars().collect();
+    for word in str.split_whitespace() {
+        for triad in word.chars().map(|x| x).collect::<Vec<char>>().windows(3) {
+            let triad_string: String = triad.iter().map(|c| c.to_string()).collect::<Vec<String>>().join("");
+            let counter = output.entry(triad_string).or_insert(0);
+            *counter += 1;
+        }
+    }
 
     output
+}
+
+pub fn analyze_string_triads_sorted(str: &str) -> Vec<(String, u32)> {
+    let result: HashMap<String, u32> = analyze_string_triads(&str);
+    let mut result_vec: Vec<(String, u32)> = result.into_iter().collect();
+    result_vec.sort_by(|a, b| b.1.cmp(&a.1));
+
+    result_vec
 }
 
 #[cfg(test)]
@@ -80,8 +95,23 @@ mod tests {
 
     #[test]
     fn test_analyze_string_triads() {
-        let output = analyze_string_triads("abcdefg hij kl m bcde");
+        let output = analyze_string_triads("abcdefg hij kl m bcde e e");
 
         assert_eq!(output["abc"], 1);
+        assert_eq!(output["bcd"], 2);
+        assert_eq!(output["cde"], 2);
+        assert_eq!(output["def"], 1);
+        assert_eq!(output["efg"], 1);
+        assert_eq!(output["hij"], 1);
+        assert_eq!(output.get("g h"), None);
+        assert_eq!(output.get("e e"), None);
     }
+
+    // #[test]
+    // fn test_analyze_string_triads_sorted() {
+    //     let output = analyze_string_triads_sorted("abcdefg hij kl m bcde e e");
+    //     assert_eq!(output[0], ("bcd".to_string(), 2));
+    //     assert_eq!(output[1], ("cde".to_string(), 2));
+    // }
+
 }
